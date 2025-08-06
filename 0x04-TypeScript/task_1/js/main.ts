720e54319fe8ba90a5bc96495c867167c80dc309
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-07 01:05:00 +0300

    task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -31,3 +31,15 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // J. Doe
