afba6988d34381c0a045cadca1449b7c38c0eb59
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-07 00:55:24 +0300

    first commit

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number; // optional attribute
+  [key: string]: any; // allows additional attributes
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false, // additional attribute
+};
+
+console.log(teacher3);
